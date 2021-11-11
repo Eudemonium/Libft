e29e43e9e1ac44c08331436678b3fd1a12bdc1c7
Joshua Agagas <jagagas@c2r5p12.hive.fi>
2021-11-11 11:00:29 +0200

    Add ft_strncat

--- ft_strncat.c
+++ ft_strncat.c
@@ -1,18 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jagagas <jagagas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/11 07:34:08 by jagagas           #+#    #+#             */
-/*   Updated: 2021/11/11 10:58:35 by jagagas          ###   ########.fr       */
+/*   Created: 2021/11/11 10:49:16 by jagagas           #+#    #+#             */
+/*   Updated: 2021/11/11 10:58:19 by jagagas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcat(char *s1, const char *s2)
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	return (ft_strncat(s1, s2, ft_strlen(s2)));
+	int	index;
+	int	offset;
+
+	index = 0;
+	offset = 0;
+	while (s1[index])
+	{
+		index++;
+		offset--;
+	}
+	while (index + offset < n)
+	{
+		s1[index] = s2[index + offset];
+		index++;
+	}
+	s1[index] = '\0';
+	return (s1);
 }
