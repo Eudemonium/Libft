f64a1e78c549b5a6102b529fa3b4408fe2d29b76
jagagas <jagagas@student.hive.fi>
2021-11-11 09:53:49 +0200

    Add ft_putstr_fd ft_putstr

---
+++ ft_putstr_fd.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jagagas <jagagas@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 12:15:01 by jagagas           #+#    #+#             */
+/*   Updated: 2021/11/08 12:16:57 by jagagas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char const *s, int fd)
+{
+	int	index;
+
+	index = 0;
+	while (s[index])
+	{
+		ft_putchar_fd(s[index], fd);
+		index++;
+	}
+}
