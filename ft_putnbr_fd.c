5cd30f0e0c9385e2c03233c27957796f255caff9
jagagas <jagagas@student.hive.fi>
2021-11-11 09:53:09 +0200

    Add headers to ft_putchar_fd ft_putchar ft_putnbr_fd ft_putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,10 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jagagas <jagagas@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 12:10:38 by jagagas           #+#    #+#             */
+/*   Updated: 2021/11/08 12:13:56 by jagagas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char num;
-
-	num = '0' + n;
-	ft_putchar_fd(num, fd);
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+	}
+	else
+	{
+		n = -n;
+	}
+	if (n > -10)
+	{
+		ft_putchar_fd('0' - n, fd);
+	}
+	else
+	{
+		ft_putnbr_fd(n / -10, fd);
+		ft_putchar_fd('0' - n % 10, fd);
+	}
 }
-
