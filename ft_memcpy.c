8ebd725364ee73fcf3dd93952f4452b970b7d22f
jagagas <jagagas@student.hive.fi>
2021-11-13 13:50:55 +0200

    Fix variable alignment ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: jagagas <jagagas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 09:59:48 by jagagas           #+#    #+#             */
-/*   Updated: 2021/11/13 10:14:38 by jagagas          ###   ########.fr       */
+/*   Updated: 2021/11/13 13:50:22 by jagagas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	index;
+	size_t			index;
 	unsigned char	*p_dst;
 	unsigned char	*p_src;
 
