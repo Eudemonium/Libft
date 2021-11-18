d730a6f1d1124428ec7168f6c5f855dbba969a1a
jagagas <jagagas@student.hive.fi>
2021-11-18 18:26:14 +0200

    Unit test ft_isalnum

--- test/ft_isalnum_test.c
+++ test/ft_isalnum_test.c
@@ -1,15 +1,67 @@
 #include "../libft.h"
 #include "libft_test.h"
 
+#ifdef assert
+#undef assert
+#endif
+
+#define assert(eval, test) \
+	if (eval) ++success; else {++failure; printf("fail: \"%c\"\n", test); }
+
 int	main(void)
 {
-	printf("###: isalnum() ft_isalnum()\n");
-	printf("000: %d %d\n", isalnum(000), ft_isalnum(000));
-	printf("177: %d %d\n", isalnum(177), ft_isalnum(177));
-	printf("55:  %d %d\n", isalnum(55), ft_isalnum(55));
-	printf("00:  %d %d\n", isalnum(00), ft_isalnum(00));
-	printf("0:   %d %d\n", isalnum(0), ft_isalnum(0));
-	printf("49:  %d %d\n", isalnum(49), ft_isalnum(49));
-	printf("128: %d %d\n", isalnum(128), ft_isalnum(128));
+	unsigned	success = 0;
+	unsigned	failure = 0;
+	char		*func = "ft_isalnum";
+	printf("===== %s =====\n", func);
+	{
+		int		num = 000;
+		int		expect = isalnum(num);
+		int		actual = ft_isalnum(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = '0';
+		int		expect = isalnum(num);
+		int		actual = ft_isalnum(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = '9';
+		int		expect = isalnum(num);
+		int		actual = ft_isalnum(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = '+';
+		int		expect = isalnum(num);
+		int		actual = ft_isalnum(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = 'a' - 1;
+		int		expect = isalnum(num);
+		int		actual = ft_isalnum(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = -'0';
+		int		expect = isalnum(num);
+		int		actual = ft_isalnum(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = 'z';
+		int		expect = isalnum(num);
+		int		actual = ft_isalnum(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = 'z' + 1;
+		int		expect = isalnum(num);
+		int		actual = ft_isalnum(num);
+		assert(expect == actual, num);
+	}
+	printf("%i of %i tests passed.\n", success, (success + failure));
 	return (0);
 }
