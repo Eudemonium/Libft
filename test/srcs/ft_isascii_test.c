b96b2e93e145e468c27c453da1dd57e8a68776fa
jagagas <jagagas@student.hive.fi>
2021-11-19 12:23:18 +0200

    Unit test ft_isascii

--- test/srcs/ft_isascii_test.c
+++ test/srcs/ft_isascii_test.c
@@ -1,15 +1,67 @@
-#include "../libft.h"
+#include "libft.h"
 #include "libft_test.h"
+
+#ifdef assert
+#undef assert
+#endif
+
+#define assert(eval, test) \
+	if (eval) ++success; else {++failure; printf("fail: \"%c\"\n", test); }
 
 int	main(void)
 {
-	printf("###: isascii() ft_isascii\n");
-	printf("000: %d %d\n", isascii(000), ft_isascii(000));
-	printf("177: %d %d\n", isascii(177), ft_isascii(177));
-	printf("178: %d %d\n", isascii(178), ft_isascii(178));
-	printf("00:  %d %d\n", isascii(00), ft_isascii(00));
-	printf("0:   %d %d\n", isascii(0), ft_isascii(0));
-	printf("127: %d %d\n", isascii(127), ft_isascii(127));
-	printf("128: %d %d\n", isascii(128), ft_isascii(128));
+	unsigned	success = 0;
+	unsigned	failure = 0;
+	char		*func = "ft_isascii";
+	printf("===== %s =====\n", func);
+	{
+		int		num = 000;
+		int		expect = isascii(num);
+		int		actual = ft_isascii(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = INT_MIN;
+		int		expect = isascii(num);
+		int		actual = ft_isascii(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = INT_MAX;
+		int		expect = isascii(num);
+		int		actual = ft_isascii(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = '+';
+		int		expect = isascii(num);
+		int		actual = ft_isascii(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = 'a';
+		int		expect = isascii(num);
+		int		actual = ft_isascii(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = 128;
+		int		expect = isascii(num);
+		int		actual = ft_isascii(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = '\n';
+		int		expect = isascii(num);
+		int		actual = ft_isascii(num);
+		assert(expect == actual, num);
+	}
+	{
+		int		num = '\t';
+		int		expect = isascii(num);
+		int		actual = ft_isascii(num);
+		assert(expect == actual, num);
+	}
+	printf("%i of %i tests passed.\n", success, (success + failure));
 	return (0);
 }
