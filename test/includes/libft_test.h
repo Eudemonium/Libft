564aa68a991266e41f34f069b4bae50ffeeff4fd
jagagas <jagagas@student.hive.fi>
2021-11-20 14:40:09 +0200

    Add standard lib headers to test

--- test/includes/libft_test.h
+++ test/includes/libft_test.h
@@ -2,9 +2,9 @@
 # define LIBFT_TEST_H
 # define INT_MIN_C "-2147483648"
 # define INT_MAX_C "2147483647"
-#include <stdio.h>
-#include <ctype.h>
-#include <limits.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <ctype.h>
+# include <limits.h>
+# include <stdlib.h>
 
 #endif
