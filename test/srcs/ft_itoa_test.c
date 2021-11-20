d262ad5719f4843437779b80bfc90eeaf53850d5
jagagas <jagagas@student.hive.fi>
2021-11-20 14:39:11 +0200

    Unit test ft_itoa

--- test/srcs/ft_itoa_test.c
+++ test/srcs/ft_itoa_test.c
@@ -1,16 +1,43 @@
-#include "../libft.h"
+#include "libft.h"
 #include "libft_test.h"
+
+#ifdef assert
+#undef assert
+#endif
+
+#define assert(eval, test) \
+	if (eval) ++success; else {++failure; printf("fail: \"%d\"\n", test); }
 
 int	main(void)
 {
-	char *str;
-	str = ft_itoa(4200);
-	ft_putstr("Result: ");
-	ft_putstr(str);
-	ft_putchar('\n');
-	str = ft_itoa(-42);
-	ft_putstr("Result: ");
-	ft_putstr(str);
-	ft_putchar('\n');
+	unsigned	success = 0;
+	unsigned	failure = 0;
+	char		*func = "ft_itoa";
+	printf("===== %s =====\n", func);
+	{
+		int		num = INT_MIN;
+		char	*expect = INT_MIN_C;
+		char	*actual = ft_itoa(num);
+		assert(strcmp(expect, actual) == 0, num);
+	}
+	{
+		int		num = INT_MAX;
+		char	*expect = INT_MAX_C;
+		char	*actual = ft_itoa(num);
+		assert(strcmp(expect, actual) == 0, num);
+	}
+	{
+		int		num = -42;
+		char	*expect = "-42";
+		char	*actual = ft_itoa(num);
+		assert(strcmp(expect, actual) == 0, num);
+	}
+	{
+		int		num = 42;
+		char	*expect = "42";
+		char	*actual = ft_itoa(num);
+		assert(strcmp(expect, actual) == 0, num);
+	}
+	printf("%i of %i tests passed.\n", success, (success + failure));
 	return (0);
 }
