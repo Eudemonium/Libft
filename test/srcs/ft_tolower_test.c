1464e0525d139acdb417537d8909aade39af698f
jagagas <jagagas@student.hive.fi>
2021-11-20 14:12:38 +0200

    Unit test ft_tolower

--- test/srcs/ft_tolower_test.c
+++ test/srcs/ft_tolower_test.c
@@ -1,26 +1,67 @@
-#include "../libft.h"
+#include "libft.h"
 #include "libft_test.h"
 
-int	main(int argc, char **argv)
-{
-	int	arg_index;
-	int	c_index;
+#ifdef assert
+#undef assert
+#endif
 
-	arg_index = 1;
-	if (argc >= 2)
+#define assert(eval, test) \
+	if (eval) ++success; else {++failure; printf("fail: \"%c\"\n", test); }
+
+int	main(void)
+{
+	unsigned	success = 0;
+	unsigned	failure = 0;
+	char		*func = "ft_tolower";
+	printf("===== %s =====\n", func);
 	{
-		while (arg_index < argc)
-		{
-			c_index = 0;
-			while (argv[arg_index][c_index])
-			{
-				ft_putchar(ft_tolower(argv[arg_index][c_index]));
-				c_index++;
-			}
-			ft_putchar(' ');
-			arg_index++;
-		}
+		int		chr = 000;
+		int		expect = tolower(chr);
+		int		actual = ft_tolower(chr);
+		assert(expect == actual, chr);
 	}
-	ft_putchar('\n');
+	{
+		int		chr = '0';
+		int		expect = tolower(chr);
+		int		actual = ft_tolower(chr);
+		assert(expect == actual, chr);
+	}
+	{
+		int		chr = 'a';
+		int		expect = tolower(chr);
+		int		actual = ft_tolower(chr);
+		assert(expect == actual, chr);
+	}
+	{
+		int		chr = 'H';
+		int		expect = tolower(chr);
+		int		actual = ft_tolower(chr);
+		assert(expect == actual, chr);
+	}
+	{
+		int		chr = 'a' - 1;
+		int		expect = tolower(chr);
+		int		actual = ft_tolower(chr);
+		assert(expect == actual, chr);
+	}
+	{
+		int		chr = -'0';
+		int		expect = tolower(chr);
+		int		actual = ft_tolower(chr);
+		assert(expect == actual, chr);
+	}
+	{
+		int		chr = 'Z';
+		int		expect = tolower(chr);
+		int		actual = ft_tolower(chr);
+		assert(expect == actual, chr);
+	}
+	{
+		int		chr = 'z' + 1;
+		int		expect = tolower(chr);
+		int		actual = ft_tolower(chr);
+		assert(expect == actual, chr);
+	}
+	printf("%i of %i tests passed.\n", success, (success + failure));
 	return (0);
 }
