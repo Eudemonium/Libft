38f7b888c93972b3d6175ac10944c18687cbc405
jagagas <jagagas@student.hive.fi>
2021-11-22 13:36:13 +0200

    Resolve ft_memmove for NULL parameters

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: jagagas <jagagas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 10:23:26 by jagagas           #+#    #+#             */
-/*   Updated: 2021/11/21 00:16:33 by jagagas          ###   ########.fr       */
+/*   Updated: 2021/11/22 11:48:23 by jagagas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	p_dst = dst;
 	p_src = src;
+	if (dst == NULL && src == NULL)
+		return (dst);
 	if (dst < src)
 		return (ft_memcpy(dst, src, len));
 	else
