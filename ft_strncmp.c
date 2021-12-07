204d69724fc1bc2a5ec6ff6e2122d8fa3e2f3d08
jagagas <jagagas@student.hive.fi>
2021-12-07 13:33:48 +0200

    Fix de-increment len in ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jagagas <jagagas@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: jagagas <jagagas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 11:41:25 by jagagas           #+#    #+#             */
-/*   Updated: 2021/11/29 15:30:47 by jagagas          ###   ########.fr       */
+/*   Updated: 2021/12/07 13:31:55 by jagagas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t len)
 {
-	while (*s1 && *s2 && len--)
+	while (*s1 && *s2 && len)
 	{
 		if (*s1 != *s2)
 			return ((unsigned char)*s1 - (unsigned char)*s2);
 		s1++;
 		s2++;
+		len--;
 	}
 	if (len == 0)
 		return (0);
