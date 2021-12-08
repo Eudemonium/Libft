aeb5080c638d0f8dda6491a6c3696edd7a77dc21
jagagas <jagagas@student.hive.fi>
2021-12-08 16:16:10 +0200

    Don't protect ft_strdup from NULL input

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jagagas <jagagas@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: jagagas <jagagas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 18:38:35 by jagagas           #+#    #+#             */
-/*   Updated: 2021/11/17 21:08:55 by jagagas          ###   ########.fr       */
+/*   Updated: 2021/12/08 15:50:43 by jagagas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char	*ft_strdup(const char *s1)
 {
 	char	*s2;
 
-	if (s1 == NULL)
-		return (NULL);
 	s2 = (char *)malloc(sizeof(char) * ft_strlen((char *)s1) + 1);
 	if (s2 == NULL)
 		return (NULL);
