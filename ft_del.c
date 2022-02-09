7be2c7151c70beb1bc3e565972f63513441a031f
jagagas <jagagas@student.hive.fi>
2022-02-09 13:03:44 +0200

    Add tests for lst functions

--- ft_del.c
+++ ft_del.c
@@ -6,7 +6,7 @@
 /*   By: jagagas <jagagas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 12:38:08 by jagagas           #+#    #+#             */
-/*   Updated: 2022/02/07 11:46:39 by jagagas          ###   ########.fr       */
+/*   Updated: 2022/02/09 12:47:54 by jagagas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,5 @@
 void	ft_del(void *content, size_t content_size)
 {
 	free(content);
-	content = NULL;
 	(void)content_size;
 }
